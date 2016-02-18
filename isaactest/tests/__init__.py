d17e0368d890b3ea58c2116dfb7b5f4077054f13
James Sharkey <jsharkey13@users.noreply.github.com>
2016-02-18 17:09:44 +0000

    Rewrite decorator to have class methods allowing tests to be run by running dependencies, add method to run all tests. List imports in that file now.

--- isaactest/tests/__init__.py
+++ isaactest/tests/__init__.py
@@ -26,11 +26,14 @@ class TestWithDependency(object):
           appear in 'Results' keys; a 'KeyError' exception will be raised.
     """
     Results = OrderedDict()
+    _Tests = OrderedDict()
+    _Dependencies = dict()
 
     def __init__(self, Name, deps=[]):
         self.Name = Name
         self.deps = deps
         self.Results[Name] = None
+        self._Dependencies[Name] = self.deps
 
     def __call__(self, test_func):
         def _decorator(*args, **kwargs):
@@ -38,7 +41,7 @@ class TestWithDependency(object):
                 log(TEST, "Test '%s'." % self.Name)
                 log(INFO, "Test has already run.")
                 return
-            if self.dependencies_met():
+            if self.dependencies_met(self.Name):
                 log(TEST, "Test '%s'." % self.Name)
                 self.Results[self.Name] = False  # If it dies; ensure this test marked as a fail!
                 result = test_func(*args, **kwargs)
@@ -55,7 +58,121 @@ class TestWithDependency(object):
                 del self.Results[self.Name]  # This moves the entry to the end,
                 self.Results[self.Name] = None  # So it is clearer which were not run.
                 return None
-        return wraps(test_func)(_decorator)
+        test = wraps(test_func)(_decorator)
+        self._Tests[self.Name] = test
+        return test
 
-    def dependencies_met(self):
-        return all([self.Results[d] for d in self.deps])
+    @classmethod
+    def dependencies_met(cls, name):
+        return all([cls.Results[d] for d in cls._Dependencies[name]])
+
+    @classmethod
+    def run_test_with_deps(cls, name, driver, inbox, Users, ISAAC_WEB, GUERRILLAMAIL, WAIT_DUR):
+        """Run a single test from the test suite, first running all dependencies.
+
+           This ideally should not be used as a stand in to run a short list of tests
+           (though it ought to work if made to). It is designed for running single
+           tests when a feature of the website has changed and only one area needs
+           testing. It avoids having to work out the dependencies by hand.
+            - 'cls' is automatically passed in because this function is decorated
+              as a classmethod. IGNORE THIS ARGUMENT.
+            - 'name' is the uppercase name of the test to run.
+            - 'driver' should be a Selenium WebDriver.
+            - 'inbox' must be a GuerrillaInbox object.
+            - 'Users' should be the TestUsers object.
+            - 'ISAAC_WEB' is the string URL of the Isaac website to be tested.
+            - 'GUERRILLAMAIL' is the string URL of GuerrillaMail.
+            - 'WAIT_DUR' is the time in seconds to wait for JavaScript to run/load.
+        """
+        # This is the superset of all arguments required by tests. Their "**kwargs"
+        # argument allows us to pass in all arguments and any extra ones will simply
+        # be ignored.
+        kwargs = {"driver": driver, "inbox": inbox, "Users": Users, "ISAAC_WEB": ISAAC_WEB,
+                  "GUERRILLAMAIL": GUERRILLAMAIL, "WAIT_DUR": WAIT_DUR}
+
+        # Run each of the dependency tests if necessary, and recursively run their
+        # dependencies. DOES NOT CHECK FOR SUCCESS of dependencies, that is left to
+        # the main decorator above.
+        for t in cls._Dependencies[name]:
+            if type(cls.Results[t]) != bool:
+                cls.run_test_with_deps(t, **kwargs)
+        cls._Tests[name](**kwargs)
+
+    @classmethod
+    def run_all_tests(cls, driver, inbox, Users, ISAAC_WEB, GUERRILLAMAIL, WAIT_DUR):
+        """Run all tests from the test suite.
+
+           This will run all defined tests, in the order set by their imports. This
+           ordering is important if all are to run successfully.
+            - 'cls' is automatically passed in because this function is decorated
+              as a classmethod. IGNORE THIS ARGUMENT.
+            - 'driver' should be a Selenium WebDriver.
+            - 'inbox' must be a GuerrillaInbox object.
+            - 'Users' should be the TestUsers object.
+            - 'ISAAC_WEB' is the string URL of the Isaac website to be tested.
+            - 'GUERRILLAMAIL' is the string URL of GuerrillaMail.
+            - 'WAIT_DUR' is the time in seconds to wait for JavaScript to run/load.
+        """
+        # This is the superset of all arguments required by tests. Their "**kwargs"
+        # argument allows us to pass in all arguments and any extra ones will simply
+        # be ignored.
+        kwargs = {"driver": driver, "inbox": inbox, "Users": Users, "ISAAC_WEB": ISAAC_WEB,
+                  "GUERRILLAMAIL": GUERRILLAMAIL, "WAIT_DUR": WAIT_DUR}
+
+        for test in cls._Tests:
+            cls._Tests[test](**kwargs)
+
+
+# Import all known tests into the namespace of this file, also avoids extra imports
+# in any file using the module. It can just import TestWithDependency from this
+# file and these imports will declare all the requisite tests.
+from .login import login
+from .questionnaire import questionnaire
+from .global_nav import global_nav
+from .logout import logout
+from .login_throttle import login_throttle
+from .login_timeout import login_timeout
+from .signup import signup
+from .welcome_email import welcome_email
+from .req_verify_emails import req_verify_emails
+from .recieve_verify_emails import recieve_verify_emails
+from .verify_link import verify_link
+from .verify_banner_gone import verify_banner_gone
+from .pwd_reset_throttle import pwd_reset_throttle
+from .recieve_pwd_reset_emails import recieve_pwd_reset_emails
+from .pwd_reset_link import pwd_reset_link
+from .reset_pwd_login import reset_pwd_login
+from .login_uppercase import login_uppercase
+from .signup_uppercase import signup_uppercase
+from .user_consistency import user_consistency
+from .user_consistency_popup import user_consistency_popup
+from .email_change import email_change
+from .email_change_emails import email_change_emails
+from .email_change_login_status import email_change_login_status
+from .admin_page_access import admin_page_access
+from .delete_user import delete_user
+from .accordion_behaviour import accordion_behavior
+from .quick_questions import quick_questions
+from .multiple_choice_questions import multiple_choice_questions
+from .numeric_q_units_select import numeric_q_units_select
+from .numeric_q_all_correct import numeric_q_all_correct
+from .numeric_q_answer_change import numeric_q_answer_change
+from .numeric_q_incorrect_unit import numeric_q_incorrect_unit
+from .numeric_q_incorrect_value import numeric_q_incorrect_value
+from .numeric_q_all_incorrect import numeric_q_all_incorrect
+from .numeric_q_incorrect_sf import numeric_q_incorrect_sf
+from .numeric_q_incorrect_sf_u import numeric_q_incorrect_sf_u
+from .numeric_q_known_wrong_ans import numeric_q_known_wrong_ans
+from .numeric_q_known_wrong_sf import numeric_q_known_wrong_sf
+from .numeric_q_help_popup import numeric_q_help_popup
+from .answer_saved_login import answer_saved_login
+from .tab_behaviour import tab_behavior
+from .back_to_board import back_to_board
+from .filter_behaviour import filter_behaviour
+from .filter_by_concept import filter_by_concept
+from .admin_stats_summary import admin_stats_summary
+from .admin_stats_analytics import admin_stats_analytics
+from .admin_stats_gameboards import admin_stats_gameboards
+from .admin_stats_schools import admin_stats_schools
+from .admin_user_search import admin_user_search
+from .non_admin_user_search import non_admin_user_search
--- selenium_tester.py
+++ selenium_tester.py
@@ -8,57 +8,6 @@ from isaactest.utils.log import log, INFO, ERROR, start_testing, end_testing
 from isaactest.utils.initialisation import define_users, start_selenium
 from isaactest.tests import TestWithDependency
 from isaactest.utils.isaac import User, TestUsers  # Need this for the pickle loading
-# Test Imports:
-from isaactest.tests.login import login
-from isaactest.tests.questionnaire import questionnaire
-from isaactest.tests.global_nav import global_nav
-from isaactest.tests.logout import logout
-from isaactest.tests.login_throttle import login_throttle
-from isaactest.tests.login_timeout import login_timeout
-from isaactest.tests.signup import signup
-from isaactest.tests.welcome_email import welcome_email
-from isaactest.tests.req_verify_emails import req_verify_emails
-from isaactest.tests.recieve_verify_emails import recieve_verify_emails
-from isaactest.tests.verify_link import verify_link
-from isaactest.tests.verify_banner_gone import verify_banner_gone
-from isaactest.tests.pwd_reset_throttle import pwd_reset_throttle
-from isaactest.tests.recieve_pwd_reset_emails import recieve_pwd_reset_emails
-from isaactest.tests.pwd_reset_link import pwd_reset_link
-from isaactest.tests.reset_pwd_login import reset_pwd_login
-from isaactest.tests.login_uppercase import login_uppercase
-from isaactest.tests.signup_uppercase import signup_uppercase
-from isaactest.tests.user_consistency import user_consistency
-from isaactest.tests.user_consistency_popup import user_consistency_popup
-from isaactest.tests.email_change import email_change
-from isaactest.tests.email_change_emails import email_change_emails
-from isaactest.tests.email_change_login_status import email_change_login_status
-from isaactest.tests.admin_page_access import admin_page_access
-from isaactest.tests.delete_user import delete_user
-from isaactest.tests.accordion_behaviour import accordion_behavior
-from isaactest.tests.quick_questions import quick_questions
-from isaactest.tests.multiple_choice_questions import multiple_choice_questions
-from isaactest.tests.numeric_q_units_select import numeric_q_units_select
-from isaactest.tests.numeric_q_all_correct import numeric_q_all_correct
-from isaactest.tests.numeric_q_answer_change import numeric_q_answer_change
-from isaactest.tests.numeric_q_incorrect_unit import numeric_q_incorrect_unit
-from isaactest.tests.numeric_q_incorrect_value import numeric_q_incorrect_value
-from isaactest.tests.numeric_q_all_incorrect import numeric_q_all_incorrect
-from isaactest.tests.numeric_q_incorrect_sf import numeric_q_incorrect_sf
-from isaactest.tests.numeric_q_incorrect_sf_u import numeric_q_incorrect_sf_u
-from isaactest.tests.numeric_q_known_wrong_ans import numeric_q_known_wrong_ans
-from isaactest.tests.numeric_q_known_wrong_sf import numeric_q_known_wrong_sf
-from isaactest.tests.numeric_q_help_popup import numeric_q_help_popup
-from isaactest.tests.answer_saved_login import answer_saved_login
-from isaactest.tests.tab_behaviour import tab_behavior
-from isaactest.tests.back_to_board import back_to_board
-from isaactest.tests.filter_behaviour import filter_behaviour
-from isaactest.tests.filter_by_concept import filter_by_concept
-from isaactest.tests.admin_stats_summary import admin_stats_summary
-from isaactest.tests.admin_stats_analytics import admin_stats_analytics
-from isaactest.tests.admin_stats_gameboards import admin_stats_gameboards
-from isaactest.tests.admin_stats_schools import admin_stats_schools
-from isaactest.tests.admin_user_search import admin_user_search
-from isaactest.tests.non_admin_user_search import non_admin_user_search
 
 
 #####
@@ -97,6 +46,7 @@ except Exception:
     pass
 os.chdir("test_" + RUNDATE)
 
+
 #####
 # Start Testing:
 #####
@@ -108,56 +58,11 @@ driver, inbox = start_selenium(Users, ISAAC_WEB, GUERRILLAMAIL, WAIT_DUR)
 
 fatal_error = False
 try:
-    login(driver, Users, ISAAC_WEB, WAIT_DUR)
-    questionnaire(driver, ISAAC_WEB)
-    global_nav(driver, ISAAC_WEB, WAIT_DUR)
-    logout(driver, ISAAC_WEB, WAIT_DUR)
-    login_throttle(driver, Users, ISAAC_WEB, WAIT_DUR)
-    login_timeout(driver, Users, ISAAC_WEB, WAIT_DUR)
-    signup(driver, Users, ISAAC_WEB, WAIT_DUR)
-    welcome_email(driver, inbox, GUERRILLAMAIL)
-    req_verify_emails(driver, ISAAC_WEB, WAIT_DUR)
-    recieve_verify_emails(driver, inbox, GUERRILLAMAIL)
-    verify_link(driver, inbox, ISAAC_WEB, GUERRILLAMAIL, WAIT_DUR)
-    verify_banner_gone(driver, ISAAC_WEB, WAIT_DUR)
-    pwd_reset_throttle(driver, Users, ISAAC_WEB, WAIT_DUR)
-    recieve_pwd_reset_emails(driver, inbox, GUERRILLAMAIL, WAIT_DUR)
-    pwd_reset_link(driver, inbox, Users, ISAAC_WEB, GUERRILLAMAIL, WAIT_DUR)
-    reset_pwd_login(driver, Users, ISAAC_WEB, WAIT_DUR)
-    login_uppercase(driver, Users, ISAAC_WEB, WAIT_DUR)
-    signup_uppercase(driver, Users, ISAAC_WEB, WAIT_DUR)
-    user_consistency(driver, Users, ISAAC_WEB, WAIT_DUR)
-    user_consistency_popup(driver, ISAAC_WEB, WAIT_DUR)
-    email_change(driver, Users, ISAAC_WEB, WAIT_DUR)
-    email_change_emails(driver, inbox, Users, GUERRILLAMAIL, WAIT_DUR)
-    email_change_login_status(driver, Users, ISAAC_WEB, WAIT_DUR)
-    admin_page_access(driver, Users, ISAAC_WEB, WAIT_DUR)
-    accordion_behavior(driver, ISAAC_WEB, WAIT_DUR)
-    tab_behavior(driver, ISAAC_WEB, WAIT_DUR)
-    quick_questions(driver, ISAAC_WEB, WAIT_DUR)
-    multiple_choice_questions(driver, ISAAC_WEB, WAIT_DUR)
-    numeric_q_units_select(driver, ISAAC_WEB, WAIT_DUR)
-    numeric_q_all_correct(driver, ISAAC_WEB, WAIT_DUR)
-    numeric_q_answer_change(driver, ISAAC_WEB, WAIT_DUR)
-    numeric_q_incorrect_unit(driver, ISAAC_WEB, WAIT_DUR)
-    numeric_q_incorrect_value(driver, ISAAC_WEB, WAIT_DUR)
-    numeric_q_all_incorrect(driver, ISAAC_WEB, WAIT_DUR)
-    numeric_q_incorrect_sf(driver, ISAAC_WEB, WAIT_DUR)
-    numeric_q_incorrect_sf_u(driver, ISAAC_WEB, WAIT_DUR)
-    numeric_q_known_wrong_ans(driver, ISAAC_WEB, WAIT_DUR)
-    numeric_q_known_wrong_sf(driver, ISAAC_WEB, WAIT_DUR)
-    numeric_q_help_popup(driver, ISAAC_WEB, WAIT_DUR)
-    answer_saved_login(driver, Users, ISAAC_WEB, WAIT_DUR)
-    delete_user(driver, Users, ISAAC_WEB, WAIT_DUR)
-    back_to_board(driver, ISAAC_WEB, WAIT_DUR)
-    filter_behaviour(driver, ISAAC_WEB, WAIT_DUR)
-    filter_by_concept(driver, ISAAC_WEB, WAIT_DUR)
-    admin_stats_summary(driver, Users, ISAAC_WEB, WAIT_DUR)
-    admin_stats_analytics(driver, ISAAC_WEB, WAIT_DUR)
-    admin_stats_gameboards(driver, ISAAC_WEB, WAIT_DUR)
-    admin_stats_schools(driver, ISAAC_WEB, WAIT_DUR)
-    admin_user_search(driver, Users, ISAAC_WEB, WAIT_DUR)
-    non_admin_user_search(driver, Users, ISAAC_WEB, WAIT_DUR)
+    # Use the class method to run all tests in order they're designed to run in:
+    TestWithDependency.run_all_tests(driver, inbox, Users, ISAAC_WEB, GUERRILLAMAIL, WAIT_DUR)
+
+    # Comment out the above line and replace with below line to run a specific test:
+    # TestWithDependency.run_test_with_deps("LOGIN", driver, inbox, Users, ISAAC_WEB, GUERRILLAMAIL, WAIT_DUR)
 except Exception, e:
     fatal_error = True
     log(ERROR, "FATAL ERROR! %s:'%s'!" % (type(e).__name__, e.message))
